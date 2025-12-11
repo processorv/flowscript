6bed461bde821b1ad8b1fde120069588b335ca59
processorv <165093262+processorv@users.noreply.github.com>
2025-12-11 05:03:59 -0600

    Delete my-supabase-app directory

--- my-supabase-app/app/database/types.ts
+++
@@ -1,4 +0,0 @@
-export type {
-  AuthSession as SupabaseAuthSession,
-  SupabaseClient,
-} from "@supabase/supabase-js";
--- my-supabase-app/app/modules/auth/mutations/delete-auth-account.server.ts
+++
@@ -1,5 +0,0 @@
-import { getSupabaseAdmin } from "~/database";
-
-export async function deleteAuthAccount(userId: string) {
-  return getSupabaseAdmin().auth.admin.deleteUser(userId);
-}
